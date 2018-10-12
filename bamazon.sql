3172ba0ea235f974a70061fbd97c5d2584e6cafa
Dylan Burkey <dylanburkey@outlook.com>
2018-10-12 14:09:14 -0400

    Manager functions and schema

---
+++ bamazon.sql
@@ -0,0 +1,45 @@
+/*
+ Navicat MySQL Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50638
+ Source Host           : localhost
+ Source Database       : bamazon
+
+ Target Server Type    : MySQL
+ Target Server Version : 50638
+ File Encoding         : utf-8
+
+ Date: 10/12/2018 14:08:36 PM
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+--  Table structure for `departments`
+-- ----------------------------
+DROP TABLE IF EXISTS `departments`;
+CREATE TABLE `departments` (
+  `department_id` int(11) NOT NULL AUTO_INCREMENT,
+  `department_name` varchar(45) NOT NULL,
+  `over_head_costs` decimal(10,2) NOT NULL,
+  PRIMARY KEY (`department_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- ----------------------------
+--  Table structure for `products`
+-- ----------------------------
+DROP TABLE IF EXISTS `products`;
+CREATE TABLE `products` (
+  `item_id` int(11) NOT NULL AUTO_INCREMENT,
+  `product_name` varchar(45) NOT NULL,
+  `product_sales` decimal(10,2) DEFAULT '0.00',
+  `department_name` varchar(45) NOT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock_quantity` int(10) NOT NULL,
+  PRIMARY KEY (`item_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+SET FOREIGN_KEY_CHECKS = 1;
